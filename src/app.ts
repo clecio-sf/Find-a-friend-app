c088f3af072822ef7d8b59538c826daff06cbae1
Clecio Silva <clecio-sf@hotmail.com>
2025-03-29 12:32:03 -0300

    chore(app): configure global error handler and register org routes

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,12 @@
 import fastify from 'fastify'
 import { env } from './env'
 import { ZodError } from 'zod'
-<<<<<<< HEAD
 import { orgsRoutes } from './http/controllers/orgs/routes'
 
 export const app = fastify()
 
 app.register(orgsRoutes)
 
-=======
-
-export const app = fastify()
-
->>>>>>> 1d1672036a4a579a9e9a5eaefa73d30b711c5260
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
     return reply
