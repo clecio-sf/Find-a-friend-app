d6335e29ab44e71b265d5f77c511054a1a8cea7e
Clecio Silva <clecio-sf@hotmail.com>
2025-03-30 20:50:03 -0300

    test(use-case): add tests for email duplication and password hashing

--- src/use-cases/create-org-use-case.spec.ts
+++ src/use-cases/create-org-use-case.spec.ts
@@ -1,5 +1,6 @@
 import { CreateOrgUseCase } from './create-org-use-case'
 import { inMemoryCreateOrgsRepository } from '@/repositories/in-memory/in-memory-create-orgs-repository'
+import { compare } from 'bcryptjs'
 import { expect, describe, it, beforeEach } from 'vitest'
 
 let orgsRepository: inMemoryCreateOrgsRepository
@@ -27,5 +28,58 @@ describe('Create Org Use Case', () => {
     })
 
     expect(org.id).toEqual(expect.any(String))
+  })
+  it('should not be able to register with same email twice', async () => {
+    const email = 'org1@org.com'
+
+    await sut.execute({
+      name: 'org 1',
+      email,
+      password: '123456',
+      whatsapp: '77777777',
+      street: 'Street A',
+      number: '10',
+      city: 'city 1',
+      state: 'BOSTIL',
+      zipcode: '1234',
+      latitude: -14.8478133,
+      longitude: -40.867746,
+    })
+
+    await expect(() =>
+      sut.execute({
+        name: 'org 1',
+        email,
+        password: '123456',
+        whatsapp: '77777777',
+        street: 'Street A',
+        number: '10',
+        city: 'city 1',
+        state: 'BOSTIL',
+        zipcode: '1234',
+        latitude: -14.8478133,
+        longitude: -40.867746,
+      }),
+    ).rejects.toThrow('Organization with this email already exists')
+  })
+
+  it('Should hash org password upon registration', async () => {
+    const { org } = await sut.execute({
+      name: 'org 1',
+      email: 'org1@org.com',
+      password: '123456',
+      whatsapp: '77777777',
+      street: 'Street A',
+      number: '10',
+      city: 'city 1',
+      state: 'BOSTIL',
+      zipcode: '1234',
+      latitude: -14.8478133,
+      longitude: -40.867746,
+    })
+
+    const isPasswordCorrectlyHashed = await compare('123456', org.password_hash)
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
   })
 })
