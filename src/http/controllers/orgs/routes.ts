3e320b674d4481984e39dbce2e6d08fa74800745
Clecio Silva <clecio-sf@hotmail.com>
2025-03-30 20:44:17 -0300

    feat(route): protect org validation route with JWT and role middleware

--- src/http/controllers/orgs/routes.ts
+++ src/http/controllers/orgs/routes.ts
@@ -1,8 +1,19 @@
 import { FastifyInstance } from 'fastify'
 import { create } from './create'
 import { authenticate } from './authenticate'
+import { jwtVerify } from '@/http/middlewares/verify-jwt'
+import { verifyOrgRole } from '@/http/middlewares/verify-org-role'
+import { validate } from './validate'
 
 export async function orgsRoutes(app: FastifyInstance) {
   app.post('/orgs', create)
-  app.post('/sessions', authenticate)
+  app.post('/orgs/login', authenticate)
+
+  // Authenticate routes
+
+  app.get(
+    '/orgs/validate/:orgId',
+    { onRequest: [jwtVerify, verifyOrgRole('ADMIN')] },
+    validate,
+  )
 }
