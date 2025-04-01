9c556c5d9ab93861de2f31f18e3fcbd1e0d372fc
Clecio Silva <clecio-sf@hotmail.com>
2025-04-01 16:25:26 -0300

    feat(repository): add findByManyCity method to OrgsRepository interface

--- src/repositories/orgs-repository.ts
+++ src/repositories/orgs-repository.ts
@@ -3,5 +3,6 @@ import { Org, Prisma } from '@prisma/client'
 export interface OrgsRepository {
   create(data: Prisma.OrgCreateInput): Promise<Org>
   findByEmail(email: string): Promise<Org | null>
+  findByManyCity(city: string): Promise<Org[]>
   findById(id: string): Promise<Org | null>
 }
