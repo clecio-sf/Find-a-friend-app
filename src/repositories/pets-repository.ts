434ca3edfb8ed0a842ad0d1a5b7f50fd5886d1ed
Clecio Silva <clecio-sf@hotmail.com>
2025-04-01 16:22:12 -0300

    feat(repository): add interface for pets repository

---
+++ src/repositories/pets-repository.ts
@@ -0,0 +1,15 @@
+import { Pets, Prisma } from '@prisma/client'
+
+export interface PetFilters {
+  city: string
+  age?: string
+  type?: string
+  size?: string
+  color?: string
+  environment?: string
+  independencyLevel?: string
+}
+
+export interface PetsRepository {
+  create(data: Prisma.PetsUncheckedCreateInput): Promise<Pets>
+}
